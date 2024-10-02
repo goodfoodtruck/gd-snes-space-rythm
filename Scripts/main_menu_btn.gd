97e1595e887683eedc0b92a47a1af0efa1cfb561
goodfoodtruck <mielatomique@protonmail.com>
2024-10-02 23:52:41 +0200

    added label on main menu

--- Scripts/main_menu_btn.gd
+++ Scripts/main_menu_btn.gd
@@ -1,4 +1,9 @@
 extends TextureButton
 
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode in [KEY_A, KEY_X, KEY_Y, KEY_B]:
+			get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
+
 func _pressed():
 	get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
